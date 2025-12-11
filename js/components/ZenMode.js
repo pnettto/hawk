ac32f6391f7b681f25f96aaf06d97677ebef2298
Pedro Netto <pedro@pnetto.com>
2025-12-12 00:54:18 +0100

    Add quotes to zen mode

--- js/components/ZenMode.js
+++ js/components/ZenMode.js
@@ -1,7 +1,10 @@
 class ZenMode {
     getElements () {
+        const zenMode = document.getElementById('zenMode');
         return {
-            zenMode: document.getElementById('zenMode'),
+            zenMode,
+            quoteEl: zenMode.querySelector('.quote'),
+            authorEl: zenMode.querySelector('.author')
         }
     }
 
@@ -28,10 +31,36 @@ class ZenMode {
         this.listenersInitiated = true;
     }
 
+    async loadCsv() {
+        const res = await fetch('/data/quotes.csv');
+        const text = await res.text();
+
+        const lines = text
+            .split(/\r?\n/)
+            .filter(l => l.trim() !== "");    // remove empty lines
+
+        return lines;
+    }
+
+    async getRandomLine() {
+        const lines = await this.loadCsv();
+        const randomLine = lines[Math.floor(Math.random() * lines.length)];
+        const lineAtr = randomLine.split('|');
+        return lineAtr;
+    }
+
+    async showQuote () {
+        const { quoteEl, authorEl } = this.getElements();
+        const [quote, author] = await this.getRandomLine()
+        quoteEl.innerHTML = `"${quote}"`;
+        authorEl.innerHTML = author
+    }
+
     enter () {
         const { zenMode } = this.getElements();
         zenMode.classList.remove('hidden')
         document.body.style = 'overflow: hidden;';
+        this.showQuote();
     }
 
     leave() {
@@ -39,7 +68,6 @@ class ZenMode {
         zenMode.classList.add('hidden')
         document.body.style = '';
     }
-
 
     render () {
         this.setupListeners();
