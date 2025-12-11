82723a00ebd62b871e6d0b9efeee09176ff9d30f
Pedro Netto <pedro.netto@capgemini.com>
2025-12-11 13:35:28 +0100

    Fix backup feedback

--- js/components/Backup.js
+++ js/components/Backup.js
@@ -1,8 +1,15 @@
 import { backup as storageBackup } from '../utils/storage.js';
 
 class Backup {
+    getElements () {
+        return {
+            backupSaved: document.getElementById('backupSaved')
+        }
+    }
+
     setupListeners () {
         if (this.listenersInitiated) return;
+        const { main } = this.getElements();
 
         document.addEventListener('keydown', (e) => {
             const active = document.activeElement;
@@ -13,6 +20,10 @@ class Backup {
 
             if (e.key.toLowerCase() === 'b') {
                 storageBackup();
+                backupSaved.classList.remove('hidden');
+                setTimeout(() => {
+                    backupSaved.classList.add('hidden');
+                }, 500);
             }
         })
 
