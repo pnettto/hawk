914094113251db8ac7dc1a3fb91be25a4b6a614c
Pedro Netto <pedro@pnetto.com>
2025-12-14 19:53:31 +0100

    Add authorization header allowance

--- main.ts
+++ main.ts
@@ -76,7 +76,7 @@ export async function handleRequest(req: Request) {
       headers: {
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "POST, GET",
-        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization, authorization",
       },
     });
   }
