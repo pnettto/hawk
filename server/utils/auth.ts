f91e7b520bb5ff3a11c24f959838e43df75be052
Pedro Netto <pedro@pnetto.com>
2025-12-14 22:20:57 +0100

    Fix BUg

--- server/utils/auth.ts
+++ server/utils/auth.ts
@@ -1,4 +1,13 @@
+// Load environment variables
+const API_KEY = Deno.env.get("API_KEY") || '';
+
 export function isAuth(req: Request): boolean {
+    // Validate required environment variables
+    if (API_KEY === '') {
+    console.error("ERROR: Environment variables not set.");
+    throw new Error("Missing required environment variables");
+    }
+    
     const authHeader = req.headers.get('Authorization');
     if (!authHeader) return false;
 
