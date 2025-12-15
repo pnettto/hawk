c44aa227cd2ffacca4e7a11c882ccab60f22bc8f
Pedro Netto <pedro.netto@capgemini.com>
2025-12-15 10:54:31 +0100

    Switch to show work day hours by default

--- app/js/global.js
+++ app/js/global.js
@@ -1,16 +1,19 @@
-const hoursAgo = new Date(Date.now() - 2 * 60 * 60 * 1000);
-const hoursFromNow = new Date(Date.now() + 2 * 60 * 60 * 1000);
+// const hoursAgo = new Date(Date.now() - 2 * 60 * 60 * 1000);
+// const hoursFromNow = new Date(Date.now() + 2 * 60 * 60 * 1000);
 
-const earliest = new Date();
-earliest.setHours(1, 0, 0, 0);
+// const earliest = new Date();
+// earliest.setHours(1, 0, 0, 0);
 
-const latest = new Date();
-latest.setHours(23, 0, 0, 0);
+// const latest = new Date();
+// latest.setHours(23, 0, 0, 0);
 
-const hoursBegin = hoursAgo > earliest ? hoursAgo : earliest;
-const hoursEnd = hoursFromNow < latest ? hoursFromNow : latest;
+// const hoursBegin = hoursAgo > earliest ? hoursAgo : earliest;
+// const hoursEnd = hoursFromNow < latest ? hoursFromNow : latest;
 
-export let HOURS_START = hoursBegin.getHours();
-export let HOURS_END = hoursEnd.getHours();
+// export let HOURS_START = hoursBegin.getHours();
+// export let HOURS_END = hoursEnd.getHours();
+
+export let HOURS_START = 9;
+export let HOURS_END = 16;
 
 export const LOCALSTORAGE_KEY = 'hawk:data';
