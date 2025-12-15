539015db186e14b32209d002ee7b3fd21463f930
Pedro Netto <pedro@pnetto.com>
2025-12-16 00:05:30 +0100

    Fix cmd

--- server/cmd.ts
+++ server/cmd.ts
@@ -1,15 +1,18 @@
 import { corsResponse } from "./utils/cors.ts";
 
 export async function handleCmd(req: Request) {
-  const url = new URL(req.url);
-  const cmd = url.searchParams.get("cmd");
+    const url = new URL(req.url);
+    const cmd = url.searchParams.get("cmd");
 
-  if (!cmd) return corsResponse("Send ?cmd=<JS code>", { status: 500 });
+    if (!cmd) return corsResponse("Send ?cmd=<JS code>", { status: 500 });
 
-  try {
-    const result = await eval(cmd);
-    return corsResponse(JSON.stringify(result), { status: 200 });
-  } catch (e) {
-    return corsResponse(e.toString(), { status: 500 });
-  }
+    try {
+        const result = await (async () => {
+            return await eval(cmd);
+        })();
+
+        return corsResponse(JSON.stringify(result), { status: 200 });
+    } catch (e) {
+        return corsResponse(e.toString(), { status: 500 });
+    }
 }
