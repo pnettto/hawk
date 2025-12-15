6641ae3ee53dbe5ae69acaa100518f3a4f30cc09
Pedro Netto <pedro@pnetto.com>
2025-12-16 00:22:59 +0100

    Update CORS handling

--- server/utils/cors.ts
+++ server/utils/cors.ts
@@ -1,8 +1,8 @@
-export function corsResponse(body: string | null, init?: ResponseInit) {
+export function corsResponse(body: string, init: ResponseInit = {}) {
   const headers = new Headers(init?.headers);
-  headers.set("Access-Control-Allow-Origin", "*");
-  headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  headers.set("Access-Control-Allow-Methods", "POST, GET");
-  
+  headers.set("Access-Control-Allow-Origin", "*"); // allow any origin
+  headers.set("Access-Control-Allow-Headers", "Authorization, Content-Type");
+  headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+
   return new Response(body, { ...init, headers });
 }
