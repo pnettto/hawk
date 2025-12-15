b205db5147bed8e8a26b96002eb363cffec80a93
Pedro Netto <pedro@pnetto.com>
2025-12-16 00:02:59 +0100

    Iterate cmd

--- server/cmd.ts
+++ server/cmd.ts
@@ -4,12 +4,12 @@ export async function handleCmd(req: Request) {
   const url = new URL(req.url);
   const cmd = url.searchParams.get("cmd");
 
-  if (!cmd) return corsResponse("Send ?cmd=<JS code>");
+  if (!cmd) return corsResponse("Send ?cmd=<JS code>", { status: 500 });
 
   try {
     const result = await eval(cmd);
-    return corsResponse(JSON.stringify(result));
+    return corsResponse(JSON.stringify(result), { status: 200 });
   } catch (e) {
-    return corsResponse(e.toString());
+    return corsResponse(e.toString(), { status: 500 });
   }
 }
